5a23d70e657d187e4db01335d4b87f59246e5c4e
olshen <evg@wix.com>
2022-06-13 21:53:16 +0300

    Initial commit

--- src/platform.ts
+++ src/platform.ts
@@ -1,16 +1,174 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
 
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ExamplePlatformAccessory } from './platformAccessory';
+import { CrestronHomePlatformAccessory } from './platformAccessory';
+
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import https from 'https';
+
+type LightState = {
+  id: number;
+  level: number;
+  time: number;
+};
+
+type ShadeState = {
+  id: number;
+  position: number;
+};
+
+class CrestronClient {
+  private axiosClient!: AxiosInstance;
+  private apiToken: string;
+  private crestronUri: string;
+  private authKey = '';
+  private httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+  });
+
+  constructor(
+    crestronHost: string,
+    apiToken: string,
+    public readonly log: Logger) {
+
+    this.apiToken = apiToken;
+
+    this.log.debug('Configured Crestron Processor, trying to login to;', crestronHost);
+    this.crestronUri = `https://${crestronHost}/cws/api`;
+  }
+
+  public async getDevices() {
+    this.log.debug('Start discovering devices...');
+
+    try {
+      const response = await this.axiosClient.get('/devices');
+
+      //this.log.debug('Get Devices response: ', response);
+      return response.data.devices;
+    } catch (error) {
+      this.log.error('error getting devices: ', error);
+    }
+
+  }
+
+  public async getDevice(id: number) {
+
+    try {
+      const response = await this.axiosClient.get(`/devices/${id}`);
+
+      //this.log.debug('Get Device state:', response.data.devices);
+      return response.data.devices[0];
+
+    } catch (error) {
+      this.log.error('error getting lights state: ', error);
+    }
+  }
+
+  public async getShadeState(id: number){
+
+    try {
+      const response = await this.axiosClient.get(`/Shades/${id}`);
+
+      //this.log.debug('Get Device state:', response.data.devices);
+      return response.data.shades[0];
+
+    } catch (error) {
+      this.log.error('error getting lights state: ', error);
+    }
+  }
+
+  public async setShadesState(shades: ShadeState[]) {
+
+    const shadesState = { shades: shades };
+    //this.log.debug('Setting lights state:', lightsState);
+
+    try {
+      const response = await this.axiosClient.post(
+        '/Shades/SetState',
+        shadesState,
+      );
+
+      this.log.debug('Shades state changed successfully: ', response.data);
+    } catch (error) {
+      this.log.error('Error setting Shades state');
+    }
+  }
+
+  public async setLightsState(lights: LightState[]) {
+
+    const lightsState = { lights: lights };
+    //this.log.debug('Setting lights state:', lightsState);
+
+    try {
+      const response = await this.axiosClient.post(
+        '/Lights/SetState',
+        lightsState,
+      );
+
+      this.log.debug('Lights state changed successfully: ', response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        this.log.error('error changing lights state: ', error.message);
+        return error.message;
+      } else {
+        this.log.error('Unexpected error changing lights state: ', error);
+        return 'An unexpected error occurred while tried to login to Crestron';
+      }
+    }
+  }
+
+  public async login() {
+
+    this.log.debug('Starting login...');
+    try {
+      const response = await axios.get(
+        '/login',
+        {
+          httpsAgent: this.httpsAgent,
+          baseURL: this.crestronUri,
+          headers: {
+            Accept: 'application/json',
+            'Crestron-RestAPI-AuthToken': this.apiToken,
+          },
+        },
+      );
+
+      this.log.debug('Succsessfully authinticated, working with version: ', response.data.version);
+
+      const config: AxiosRequestConfig = {
+        httpsAgent: this.httpsAgent,
+        baseURL: this.crestronUri,
+        headers: {
+          'Crestron-RestAPI-AuthKey': response.data.authkey,
+        },
+      };
+
+      this.axiosClient = axios.create(
+        config,
+      );
+    } catch (error) {
+
+      if (axios.isAxiosError(error)) {
+        this.log.error('Login error: ', error.message);
+        return error.message;
+      } else {
+        this.log.error('Login unexpected error: ', error);
+        return 'An unexpected error occurred while tried to login to Crestron';
+      }
+    }
+  }
+}
 
 /**
  * HomebridgePlatform
  * This class is the main constructor for your plugin, this is where you should
  * parse the user config and discover/register accessories with Homebridge.
  */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
+export class CrestronHomePlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
+
+  public crestronClient: CrestronClient;
 
   // this is used to track restored cached accessories
   public readonly accessories: PlatformAccessory[] = [];
@@ -20,6 +178,8 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
     public readonly config: PlatformConfig,
     public readonly api: API,
   ) {
+
+    this.crestronClient = new CrestronClient(config.crestronHost, config.token, log);
     this.log.debug('Finished initializing platform:', this.config.name);
 
     // When this event is fired it means Homebridge has restored all cached accessories from disk.
@@ -32,6 +192,8 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
       this.discoverDevices();
     });
   }
+
+
 
   /**
    * This function is invoked when homebridge restores cached accessories from disk at startup.
@@ -49,21 +211,16 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
    * Accessories must only be registered once, previously created accessories
    * must not be registered again to prevent "duplicate UUID" errors.
    */
-  discoverDevices() {
+  async discoverDevices() {
 
     // EXAMPLE ONLY
     // A real plugin you would discover accessories from the local network, cloud services
     // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-    ];
+
+    await this.crestronClient.login();
+
+    const exampleDevices = await this.crestronClient.getDevices();
+    this.log.debug(JSON.stringify(exampleDevices));
 
     // loop over the discovered devices and register each one if it has not already been registered
     for (const device of exampleDevices) {
@@ -71,7 +228,7 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
       // generate a unique id for the accessory this should be generated from
       // something globally unique, but constant, for example, the device serial
       // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+      const uuid = this.api.hap.uuid.generate(device.id.toString());
 
       // see if an accessory with the same uuid has already been registered and restored from
       // the cached devices we stored in the `configureAccessory` method above
@@ -87,7 +244,7 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
 
         // create the accessory handler for the restored accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
+        new CrestronHomePlatformAccessory(this, existingAccessory);
 
         // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
         // remove platform accessories when no longer present
@@ -95,10 +252,10 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
         // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
       } else {
         // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
+        this.log.info('Adding new accessory:', device.name);
 
         // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
+        const accessory = new this.api.platformAccessory(device.name, uuid);
 
         // store a copy of the device object in the `accessory.context`
         // the `context` property can be used to store any data about the accessory you may need
@@ -106,7 +263,7 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
 
         // create the accessory handler for the newly create accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
+        new CrestronHomePlatformAccessory(this, accessory);
 
         // link the accessory to your platform
         this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,54 +1,93 @@
 import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
+import { type } from 'os';
 
-import { ExampleHomebridgePlatform } from './platform';
+import { CrestronHomePlatform } from './platform';
 
 /**
  * Platform Accessory
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
-export class ExamplePlatformAccessory {
-  private service: Service;
+export class CrestronHomePlatformAccessory {
+  private service!: Service;
 
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
+  private lightStates = {
     On: false,
     Brightness: 100,
   };
 
+  private shadeStates = {
+    CurrentPosition: 0,
+    TargetPosition: 100,
+    PositionState: this.platform.Characteristic.PositionState.STOPPED,
+  };
+
+  private crestronId = 0;
+
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: CrestronHomePlatform,
     private readonly accessory: PlatformAccessory,
   ) {
 
+    this.crestronId = accessory.context.device.id;
+
     // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Crestron Electronics')
       .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
       .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+    switch (accessory.context.device.subType) {
+      case 'Dimmer':
+      case 'Switch':
+        platform.log.debug('Adding Lightbulb', accessory.displayName, accessory.context.device);
+        // get the LightBulb service if it exists, otherwise create a new LightBulb service
+        // you can create multiple services for each accessory
+        this.service = this.accessory.getService(this.platform.Service.Lightbulb)
+          || this.accessory.addService(this.platform.Service.Lightbulb);
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
+        // set the service name, this is what is displayed as the default name on the Home app
+        // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+        this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
 
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
+        // each service must implement at-minimum the "required characteristics" for the given service type
+        // see https://developers.homebridge.io/#/service/Lightbulb
+        accessory.context.device.level > 0 ? this.lightStates.On = true : false;
+        this.lightStates.Brightness = accessory.context.device.level;
 
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
+        // register handlers for the On/Off Characteristic
+        this.service.getCharacteristic(this.platform.Characteristic.On)
+          .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
+          .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
+
+        if (accessory.context.device.subType === 'Dimmer') {
+          // register handlers for the Brightness Characteristic
+          this.service.getCharacteristic(this.platform.Characteristic.Brightness)
+            .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
+        }
+        break;
+      case 'Shade':
+        this.service = this.accessory.getService(this.platform.Service.WindowCovering)
+          || this.accessory.addService(this.platform.Service.WindowCovering);
+
+        // set the service name, this is what is displayed as the default name on the Home app
+        // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+        this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
+
+        this.service.getCharacteristic(this.platform.Characteristic.CurrentPosition)
+          .onGet(this.getShadeCurrentPosition.bind(this));
+        this.service.getCharacteristic(this.platform.Characteristic.TargetPosition)
+          .onGet(this.getShadeTargetPosition.bind(this))
+          .onSet(this.setShadeTargetPosition.bind(this));
+        this.service.getCharacteristic(this.platform.Characteristic.PositionState)
+          .onGet(this.getShadePositionState.bind(this));
+
+        break;
+      default:
+        this.platform.log.debug('Unsupported device type:', accessory.context.device.subType);
+        break;
+    }
 
     /**
      * Creating multiple services of the same type.
@@ -62,33 +101,33 @@ export class ExamplePlatformAccessory {
      */
 
     // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
+    // const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
+    //   this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
 
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
+    // const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
+    //   this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
 
-    /**
-     * Updating characteristics values asynchronously.
-     *
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     *
-     */
-    let motionDetected = false;
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
+    // /**
+    //  * Updating characteristics values asynchronously.
+    //  *
+    //  * Example showing how to update the state of a Characteristic asynchronously instead
+    //  * of using the `on('get')` handlers.
+    //  * Here we change update the motion sensor trigger states on and off every 10 seconds
+    //  * the `updateCharacteristic` method.
+    //  *
+    //  */
+    // let motionDetected = false;
+    // setInterval(() => {
+    //   // EXAMPLE - inverse the trigger
+    //   motionDetected = !motionDetected;
 
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
+    //   // push the new value to HomeKit
+    //   motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
+    //   motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
 
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
+    //   this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
+    //   this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
+    // }, 10000);
   }
 
   /**
@@ -97,8 +136,10 @@ export class ExamplePlatformAccessory {
    */
   async setOn(value: CharacteristicValue) {
     // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
+    this.lightStates.On = value as boolean;
+    const level = value ? 65535 : 0;
 
+    this.platform.crestronClient.setLightsState([{ id: this.crestronId, level: level, time: 0 }]);
     this.platform.log.debug('Set Characteristic On ->', value);
   }
 
@@ -116,14 +157,15 @@ export class ExamplePlatformAccessory {
    * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
    */
   async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
 
-    this.platform.log.debug('Get Characteristic On ->', isOn);
+    const deviceState = await this.platform.crestronClient.getDevice(this.crestronId);
 
+    this.platform.log.debug('Get Light state for:', this.accessory.displayName); //, deviceState);
+    const isOn = deviceState.level > 0 ? true : false;
+
+    this.lightStates.On = isOn;
     // if you need to return an error to show the device as "Not Responding" in the Home app:
     // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-
     return isOn;
   }
 
@@ -133,9 +175,59 @@ export class ExamplePlatformAccessory {
    */
   async setBrightness(value: CharacteristicValue) {
     // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
+    this.lightStates.Brightness = value as number;
 
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
+    this.platform.log.debug('Set Brightness -> ', value);
   }
 
+  async getShadeCurrentPosition(): Promise<CharacteristicValue>{
+
+    this.platform.log.debug('Get Shade current position called for', this.accessory.displayName);
+    const currentState = await this.platform.crestronClient.getShadeState(this.crestronId);
+
+    const position = this.crestronPositionToPercentage(currentState.position);
+    this.shadeStates.CurrentPosition = position;
+
+    this.platform.log.debug('Shade current position for', this.accessory.displayName, currentState, position);
+    return position;
+  }
+
+  async getShadePositionState(): Promise<CharacteristicValue>{
+
+    this.platform.log.debug('Get Shade position state called for', this.accessory.displayName);
+
+    return 1; // this.shadeStates.PositionState;
+  }
+
+  async setShadeTargetPosition(value: CharacteristicValue){
+
+    const nValue = Number(value);
+    this.platform.log.debug('Set Shade target position called for: ', this.accessory.displayName, value);
+    this.shadeStates.TargetPosition = nValue;
+
+    this.platform.crestronClient.setShadesState([{id: this.crestronId, position: this.percentageToCrestronRangeValue(nValue)}]);
+  }
+
+  async getShadeTargetPosition(): Promise<CharacteristicValue>{
+
+    this.platform.log.debug('Get Shade targert position called for', this.accessory.displayName);
+    return this.shadeStates.TargetPosition;
+  }
+
+  crestronPositionToPercentage(value: number): number{
+    if(value > 0){
+      return Math.round((value / 65535) * 100);
+    }
+    return value;
+  }
+
+  percentageToCrestronRangeValue(value: number): number{
+
+    if(value === 0){
+      return 0;
+    }
+    return (65535 * value) / 100;
+  }
 }
+
+
